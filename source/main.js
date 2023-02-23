1e6e8fb3909431b3294316bac0ff092e7e82573f
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2023-02-23 14:55:21 +0100

    Updated Main

--- source/main.js
+++ source/main.js
@@ -1,15 +1,38 @@
 const Eris = require("eris")
+const { Config } = require('./config.js');
+const { Logger } = require('./logger.js');
 
-const bot = new Eris("BOT TOKEN", {
+// CTRL + C
+// Kill Process Event
+process.on("SIGINT", function () {
+    Logger.LogInfo("Shutting Down...");
+    bot.disconnect();
+    Logger.LogInfo("Shut Down!")
+    process.exit();
+});
+
+const bot = new Eris(Config.botToken, {
     intents: []
 });
 
-bot.on("ready", async () => {
-    console.log("Blossom is Ready!");
+// Discord Bot Ready Event
+bot.on("ready", () => {
+    Logger.LogInfo("Ready Event Called!");
+    Logger.LogInfo("User: " + bot.user.username);
+    Logger.LogInfo("Startup Time: " + bot.startTime)
+
+     // Set Bot Status
+    bot.editStatus("online", {
+        "name": "Hello World!",
+        "type": 0,
+        "url": "https://github.com/realQuartzi/Blossom"
+    });
 });
 
+// Discord Bot Error Event
 bot.on("error", (err) => {
-    console.error(err);
+    Logger.LogError(err);
 });
 
+// Connect ^^
 bot.connect();
